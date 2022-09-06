fe436d44c91387c83c20a8c5be1d0e2d1b484a11
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-06 17:12:29 +0430

    styles added to ContactList component

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,17 +1,30 @@
+import styles from "./ContactList.module.css";
+import { FaTrashAlt, FaUserAlt } from "react-icons/fa";
+import { IconContext } from "react-icons";
+
 const ContactList = ({ contacts, onDelete }) => {
   return (
-    <>
+    <section className={styles.contactList}>
       {contacts.map((contact) => {
         const { name, email, id } = contact;
         return (
-          <div key={id}>
-            <p>Name : {name}</p>
-            <p>Email: {email}</p>
-            <button onClick={() => onDelete(id)}>Delete</button>
+          <div key={id} className={styles.item}>
+            <div className={styles.item__content}>
+              <IconContext.Provider value={{ className: "userIcon" }}>
+                <FaUserAlt />
+              </IconContext.Provider>
+              <div className={styles.item__text}>
+                <p>Name : {name}</p>
+                <p>Email: {email}</p>
+              </div>
+            </div>
+            <IconContext.Provider value={{ className: "deleteContactIcon" }}>
+              <FaTrashAlt onClick={() => onDelete(id)} />
+            </IconContext.Provider>
           </div>
         );
       })}
-    </>
+    </section>
   );
 };
 
