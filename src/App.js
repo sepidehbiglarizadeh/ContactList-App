1b520c255d7837753d68760e45daa4b6cfded033
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-09-06 17:31:40 +0430

    save contacts in localStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddContact from "./components/AddContact/AddContact";
 import ContactList from "./components/ContactList/ContactList";
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts) setContacts(savedContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContactHandler = (contact) => {
     setContacts([
